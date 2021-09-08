9574dcb2fa623447af4ed15c95b828bbb2425cfd
siryu-saito <saito.s.ryu@gmail.com>
2021-09-08 23:43:43 +0900

    勇者クラスの定義

---
+++ main.rb
@@ -0,0 +1,38 @@
+class Brave
+  attr_accessor :name, :hp, :offense, :defense
+
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+end
+
+brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+brave2 = Brave.new(name: "セフィロス", hp: 1000, offense: 200, defense: 200)
+brave3 = Brave.new(name: "マリオ", hp: 700, offense: 50, defense: 100)
+
+
+puts "NAME: #{brave.name}"
+puts "HP: #{brave.hp}"
+puts "OFFENSE: #{brave.offense}"
+puts "DEFENSE: #{brave.defense}"
+
+brave.hp -= 30
+
+puts "#{brave.name}はダメージを受けた！残りのHPは#{brave.hp}だ"
+
+puts <<~TEXT
+  NAME: #{brave2.name}
+  HP: #{brave2.hp}
+  OFFENSE: #{brave2.offense}
+  DEFENSE: #{brave3.defense}
+TEXT
+
+puts <<~TEXT
+  NAME: #{brave3.name}
+  HP: #{brave3.hp}
+  OFFENSE: #{brave3.offense}
+  DEFENSE: #{brave3.defense}
+TEXT
